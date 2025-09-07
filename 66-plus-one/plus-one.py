72accbb506c4d5561debb31070c72ffc39065703
Kunal Sahjwani <kunal.sahjwani99@gmail.com>
2025-09-07 03:08:53 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (49.31%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,7 +1,21 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        num = int("".join(map(str, digits)))  # convert to integer
-        num += 1
-        digits = [int(x) for x in str(num)]
-        return digits
+        # Start from the last digit
+        # for i in range(len(digits) - 1, -1, -1):
+        #     if digits[i] < 9:
+        #         digits[i] += 1
+        #         return digits  # Done, no carry needed
+        #     digits[i] = 0  # Set to 0 and carry to next digit
         
+        # # If all digits were 9, we need an extra 1 at the front
+        # return [1] + [0] * len(digits)
+
+
+        right = len(digits) -1
+        for i in range(right, -1 , -1):
+            if digits[i]<9:
+                digits[i]+=1
+                return digits
+            digits[i] =0
+
+        return [1] + [0] * len(digits)
